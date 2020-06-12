c73c8fb9f385972e6d3a8a3bda650a94dea83a1a
Go Su Hyun <tnqkf1010@naver.com>
2020-06-12 21:32:14 +0900

    시작 화면과 Pause메뉴(미완성)

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scripts.Manager
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        [Header("시작 화면 씬")] public string StartSceneName;
+        [Header("일시정지 캔버스")] public GameObject PauseMenuCanvas;
+        public static bool isPause = false; //메뉴 호출 시 true
+
+        public void BackToMenu()
+        {
+            SceneManager.LoadScene(StartSceneName);
+        }
+
+        void Start()
+        {
+
+        }
+
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (!isPause)
+                    CallMenu();
+                else
+                    CloseMenu();
+            }
+        }
+
+        private void CallMenu()
+        {
+            isPause = true;
+            PauseMenuCanvas.SetActive(true);
+        }
+
+        private void CloseMenu()
+        {
+            isPause = false;
+            PauseMenuCanvas.SetActive(false);
+        }
+    }
+}
